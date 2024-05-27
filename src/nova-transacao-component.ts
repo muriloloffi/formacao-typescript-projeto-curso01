260dd8e68b2288dfdc17e59312dbd92bcab9ca2c
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-05-27 16:32:10 -0300

    adiciona formatação de valores e datas

--- src/nova-transacao-component.ts
+++ src/nova-transacao-component.ts
@@ -34,7 +34,10 @@ elementoFormulario.addEventListener("submit", function (event) {
     return;
   }
 
-  elementoSaldo.textContent = saldo.toString();
+  elementoSaldo.textContent = saldo.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
 
   const novaTransacao: Transacao = {
     tipoTransacao: tipoTransacao,
