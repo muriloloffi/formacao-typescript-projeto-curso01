de186cc742bdd2e26e4b328dbea0064a5f9550cd
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-05-28 10:04:08 -0300

    formatacao de transacao

--- src/formatters.ts
+++ src/formatters.ts
@@ -1,21 +1,37 @@
 function formatarMoeda(valor: number): string {
-    return valor.toLocaleString("pt-br", {
-        style: "currency", currency: "BRL"
-    });
+  return valor.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
 
-function formatarData(data: Date, formato: FormatoData = FormatoData.PADRAO): string {
-    if (formato === FormatoData.DIA_SEMANA_DIA_MES_ANO){
-        return data.toLocaleDateString("pt-br", {
-            weekday: "long",
-            day: "2-digit",
-            month: "2-digit",
-            year: "numeric"
-        });
-    }
-    else if (formato === FormatoData.DIA_MES) {
-        return data.toLocaleDateString("pt-br", {
-             day: "2-digit", month: "2-digit"
-        })
-    }
+function formatarData(
+  data: Date,
+  formato: FormatoData = FormatoData.PADRAO
+): string {
+  if (formato === FormatoData.DIA_SEMANA_DIA_MES_ANO) {
+    return data.toLocaleDateString("pt-br", {
+      weekday: "long",
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+    });
+  } else if (formato === FormatoData.DIA_MES) {
+    return data.toLocaleDateString("pt-br", {
+      day: "2-digit",
+      month: "2-digit",
+    });
+  } else if (formato === FormatoData.PADRAO) {
+    return data.toLocaleDateString("pt-br");
+  }
+}
+
+function formatarTransacao(
+  transacao: Transacao,
+  formato: FormatoData = FormatoData.PADRAO
+): string {
+  let dataFormatada = formatarData(transacao.data, formato);
+  console.log(transacao.data);
+  let valorFormatado = formatarMoeda(transacao.valor);
+  return `${dataFormatada} - ${valorFormatado}`;
 }
